b3f8dbce453c5c71eb39a461b535396a9113b68d
Max <56233430+Heavy32@users.noreply.github.com>
2021-02-24 19:54:44 +0500

    Update TableCreation.sql

--- TableCreation.sql
+++ TableCreation.sql
@@ -9,7 +9,7 @@ CREATE TABLE Users (
            Role INT NOT NULL
 );
 
-CREATE TABLE Pets (
+CREATE TABLE Cats (
 	     Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	   Name VARCHAR(50),
        Owner_Id INT NOT NULL FOREIGN KEY REFERENCES Users
@@ -18,26 +18,26 @@ CREATE TABLE Pets (
 CREATE TABLE FeedTime (
 	     Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	    User_Id INT NOT NULL FOREIGN KEY REFERENCES Users,
-		 Pet_Id INT NOT NULL FOREIGN KEY REFERENCES Pets,
+		 Cat_Id INT NOT NULL FOREIGN KEY REFERENCES Cats,
 	  Feed_Time DATETIME2(7)
 );
 
-CREATE TABLE UsersPets (
+CREATE TABLE UsersCats (
 	     Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	    User_Id INT NOT NULL FOREIGN KEY REFERENCES Users,
-		 Pet_Id INT NOT NULL FOREIGN KEY REFERENCES Pets,
+		 Cat_Id INT NOT NULL FOREIGN KEY REFERENCES Cats,
 );
 
-CREATE TABLE Statistics (
+CREATE TABLE CatStatistics (
 	       Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	     Name nvarchar(50) NOT NULL,
   Description nvarchar(250) NOT NULL,
 SQLExpression nvarchar(max) NOT NULL
 );
 
-INSERT INTO Statistics (Name, Description, SqlExpression) VALUES ('DifferenceInFeeding', 'Return the difference between weekends and non-weekends feeding' , 
-'Select Pet_Id, 
+INSERT INTO CatStatistics (Name, Description, SqlExpression) VALUES ('DifferenceInFeeding', 'Return the difference between weekends and non-weekends feeding' , 
+'Select Cat_Id, 
 CONVERT(decimal(4, 1), ROUND(COUNT(CASE WHEN DATEPART(W, Feed_Time) = 6 OR DATEPART(W, Feed_Time) = 7 THEN 0 END) / 2. , 2)) - CONVERT(decimal(4, 1), 
 ROUND(COUNT(CASE WHEN DATEPART(W, Feed_Time) != 6 AND DATEPART(W, Feed_Time) != 7 THEN 0 END) / 5. , 2)) as Diff
 FROM FeedTime 
-GROUP BY Pet_Id;');
+GROUP BY Cat_Id;');
